e51da054d6fd6586802ee32f41c397e97f8f6fa3
sunhyekim <kitty020103@naver.com>
2024-04-02 13:01:32 +0900

    알람 시 회원정보 DB 저장 기능 구현

---
+++ capstone_back/demo/src/main/java/com/example/demo/dto/AlarmLogDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AlarmLogDto {
+    private String clientId;
+    private String date;
+    private String time;
+}
---
+++ capstone_back/demo/src/main/java/com/example/demo/service/AlarmLogService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.AlarmLogDto;
+import com.example.demo.entity.AlarmLog;
+import com.example.demo.repository.AlarmLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Service
+public class AlarmLogService {
+    private final AlarmLogRepository alarmLogRepository;
+
+    @Autowired
+    public AlarmLogService(AlarmLogRepository alarmLogRepository) {
+        this.alarmLogRepository = alarmLogRepository;
+    }
+
+    // 알람 로그 저장 메소드
+    public AlarmLog saveAlarmLog(AlarmLogDto alarmLogDto) {
+        AlarmLog alarmLog = new AlarmLog();
+
+        // Id, 날짜, 시간 설정
+        alarmLog.setClientId((alarmLogDto.getClientId()));
+        alarmLog.setDate(LocalDate.parse(alarmLogDto.getDate()));
+        alarmLog.setTime(LocalTime.parse(alarmLogDto.getTime()));
+
+        // 알람 로그 저장
+        return alarmLogRepository.save(alarmLog);
+    }
+}
