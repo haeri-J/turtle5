b1b039d71ff97164c1a3c7c51e9d8ff1f36c2f46
DaEunKwon0109 <113701669+DaEunKwon0109@users.noreply.github.com>
2024-04-06 19:52:59 +0900

    update ClientService_2

--- capstone_back/demo/src/main/java/com/example/demo/service/ClientService.java
+++ capstone_back/demo/src/main/java/com/example/demo/service/ClientService.java
@@ -26,7 +26,8 @@ public class ClientService {
 
         // 이메일이 시스템에 이미 존재하는 경우
         if(!isEmailUnique(dto.getEmail())) {
-            throw new IllegalArgumentException("이미 존재하는 이메일이므로 다른 이메일 아이디를 사용해 주세요.");        }
+            throw new IllegalArgumentException("이미 존재하는 이메일이므로 다른 이메일 아이디를 사용해 주세요.");
+        }
 
         // 비밀번호 해싱
         String hashedPassword = bCryptPasswordEncoder.encode(dto.getPassword());
@@ -40,7 +41,7 @@ public class ClientService {
         if (client.getClientId() != null) { //아직 회원가입이니깐 id에 널값이 들어가 있어야함
             return null;
         }
-        //.save는 내장함수로 dto에 저장된 내용을 엔티티 형태로 디비에 넣어줌(이때 실제 id 할당)
+        //.save는 내장함수로 엔티티 형태로 디비에 넣어줌(이때 실제 id 할당)
         return clientRepository.save(client);
     }
     // 클라이언트 등록 전 isEmailUnique() 메소드를 이용하여 제공된 이메일 주소가 고유한지 확인
