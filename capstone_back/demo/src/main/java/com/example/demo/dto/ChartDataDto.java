5bbf1bd8f3a58b9539a118b7887185824fbf5193
haeri-J <115974764+haeri-J@users.noreply.github.com>
2024-04-13 16:45:09 +0900

    생성자 추가

--- capstone_back/demo/src/main/java/com/example/demo/dto/ChartDataDto.java
+++ capstone_back/demo/src/main/java/com/example/demo/dto/ChartDataDto.java
@@ -1,14 +1,18 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.DayOfWeek;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class ChartDataDto { // 클라이언트에게 시간대별 사용량 및 관련 상세정보 전달
-    
+
     private DayOfWeek dayOfWeek; // 요일
     private long webcamDuration; // 웹캠 실행 시간(분 단위)
     private long alarmCount; // 알람 발생 횟수
