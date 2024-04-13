7b882b2aacdf238ba0179e41856e9565c4e9ca7c
haeri-J <115974764+haeri-J@users.noreply.github.com>
2024-04-13 16:38:08 +0900

    차트데이터dto 업데이트

--- capstone_back/demo/src/main/java/com/example/demo/dto/ChartDataDto.java
+++ capstone_back/demo/src/main/java/com/example/demo/dto/ChartDataDto.java
@@ -3,9 +3,13 @@ package com.example.demo.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.DayOfWeek;
+
 @Getter
 @Setter
 public class ChartDataDto { // 클라이언트에게 시간대별 사용량 및 관련 상세정보 전달
-    private String date; // 날짜
-    private int alarm_freq; // 알림 빈도수
+    
+    private DayOfWeek dayOfWeek; // 요일
+    private long webcamDuration; // 웹캠 실행 시간(분 단위)
+    private long alarmCount; // 알람 발생 횟수
 }
