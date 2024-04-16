5436e3a5c3d961c217829afe92f23df030e63af6
sunhyekim <kitty020103@naver.com>
2024-04-16 12:43:33 +0900

    security config 수정

--- capstone_back/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ capstone_back/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,38 +1,38 @@
-package com.example.demo.config;
-
-
-import com.example.demo.entity.UserRole;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-@Configuration
-@EnableWebSecurity
-
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // CSRF 보호 기능 활성화 (기본 설정을 사용하므로 별도로 명시하지 않음)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/info").authenticated()
-                        .requestMatchers("/api/admin/**").hasAuthority(UserRole.ADMIN.name())
-                        .anyRequest().permitAll())
-                .formLogin(form -> form
-                        .usernameParameter("loginId")
-                        .passwordParameter("password")
-                        .loginPage("/api/login2")
-                        .defaultSuccessUrl("/api")
-                        .failureUrl("/api/login2?error=true"))
-                .logout(logout -> logout
-                        .logoutUrl("/api/logout")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID"))
-                .exceptionHandling(exception -> exception
-                        .accessDeniedPage("/api/access-denied")); // 접근 거부 페이지 설정 (옵션)
-
-        return http.build();
-    }
-}
+//package com.example.demo.config;
+//
+//
+//import com.example.demo.entity.UserRole;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//
+//@Configuration
+//@EnableWebSecurity
+//
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                // CSRF 보호 기능 활성화 (기본 설정을 사용하므로 별도로 명시하지 않음)
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/api/info").authenticated()
+//                        .requestMatchers("/api/admin/**").hasAuthority(UserRole.ADMIN.name())
+//                        .anyRequest().permitAll())
+//                .formLogin(form -> form
+//                        .usernameParameter("loginId")
+//                        .passwordParameter("password")
+//                        .loginPage("/api/login2")
+//                        .defaultSuccessUrl("/api")
+//                        .failureUrl("/api/login2?error=true"))
+//                .logout(logout -> logout
+//                        .logoutUrl("/api/logout")
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID"))
+//                .exceptionHandling(exception -> exception
+//                        .accessDeniedPage("/api/access-denied")); // 접근 거부 페이지 설정 (옵션)
+//
+//        return http.build();
+//    }
+//}
--- capstone_back/demo/src/main/java/com/example/demo/entity/User.java
+++ capstone_back/demo/src/main/java/com/example/demo/entity/User.java
@@ -7,25 +7,55 @@ import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
 @Entity
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String loginId;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password")
     private String password;
-    private String nickname;
 
-    private UserRole role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
 
-    // OAuth 로그인에 사용
-    private String provider;
-    private String providerId;
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
-
