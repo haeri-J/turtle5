ae97518267f69978adc4e75e4ed5f905d3ab07f5
haeri-J <115974764+haeri-J@users.noreply.github.com>
2024-05-05 22:45:39 +0900

    percentagedto 수정

--- capstone_back/demo/src/main/java/com/example/demo/dto/PosturePercentageDto.java
+++ capstone_back/demo/src/main/java/com/example/demo/dto/PosturePercentageDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 public class PosturePercentageDto {
 
-    private Long clientId;
+    private String name;
     private int posturePercentage;//현재 사용자의 자세비율 퍼센티지
     private int rankPercentage;//상위 랭크 퍼센티지
 
