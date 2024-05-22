2bd02a83ffcc9665bcd3698d4e4b40def5eed72a
sunhyekim <kitty020103@naver.com>
2024-05-22 21:42:33 +0900

    Update WebConfig.java

--- capstone_back/demo/src/main/java/com/example/demo/config/WebConfig.java
+++ capstone_back/demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("https://docturtle.site", "http://localhost:3000/") // 여기서는 example.com을 허용된 출처로 설정합니다. 실제 리액트 앱이 호스팅되는 도메인 주소를 사용하세요.
-                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용된 HTTP 메서드
-                .allowCredentials(true); // 쿠키를 포함한 요청 허용
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // 모든 경로에 대해
+//                .allowedOrigins("https://docturtle.site", "http://localhost:3000/") // 여기서는 example.com을 허용된 출처로 설정합니다. 실제 리액트 앱이 호스팅되는 도메인 주소를 사용하세요.
+//                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용된 HTTP 메서드
+//                .allowCredentials(true); // 쿠키를 포함한 요청 허용
+//    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken");
+                //.allowCredentials(true);
+            }
+        };
     }
 }
